432e69b7806f5ef781251b6d2aabe525a2bd53f5
Samuel Herzog <samuel.herzog@salzburg.at>
2012-01-31 01:25:27 +0100

    added sql for server setup

---
+++ scripts/setup.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE `album` (
+    id int(11) NOT NULL auto_increment,
+    artist varchar(100) NOT NULL,
+    title varchar(100) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO album (artist, title)
+    VALUES ('Coldplay', 'Mylo Xyloto');
+INSERT INTO album (artist, title)
+    VALUES ('Noel Gallagher', 'Noel Gallagher\'s High Flying Birds!');
+INSERT INTO album (artist, title)
+    VALUES ('Adele', '21');
+INSERT INTO album (artist, title)
+    VALUES ('Matt Cardle', 'Letters');
+INSERT INTO album (artist, title)
+    VALUES ('Steps', 'The Ultimate Collection');
